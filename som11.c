751ea33a6a71e48d42c7f896ad3ea72ba24f3d19
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2017-09-04 21:43:04 +0530

    Update som11.c

--- som11.c
+++ som11.c
@@ -2,15 +2,15 @@
 int main()
 {
 int base,expo,res=1;
-printf("Enter a base number: ");
-scanf("%d", &base);
-printf("Enter an exponent: ");
-scanf("%d", &expo);
-while (expo!= 0)
+printf("Enter a base number");
+scanf("%d",&base);
+printf("Enter an exponent");
+scanf("%d",&expo);
+while (expo!=0)
 {
 res*= base;
 --expo;
 }
-printf("Answer = %d", res);
+printf("Answer=%d",res);
 return 0;
 }
